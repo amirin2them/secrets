4294105d4a08c3d06e8b792fce8f01d2f9de66ad
amirin2them <shadow@salch55.uu.me>
2022-12-25 14:26:38 +0400

    Encrypt with salt and many rounds (previous commit was hasing only)

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bP = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
 const app = express();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // using ejs for templating our app, must have ejs files in views folder
 app.set("view engine", "ejs");
@@ -37,37 +38,44 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
-  newUser.save((err) => {
-    if (!err) {
-      res.send(
-        "New user of email " +
-          req.body.username +
-          " is registered successfully!"
-      );
-    } else {
-      res.send(err);
-    }
+  bcrypt.hash(req.body.password, saltRounds,(err,hash)=>{
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save((err) => {
+      if (!err) {
+        res.send(
+          "New user of email " +
+            req.body.username +
+            " is registered successfully!"
+        );
+      } else {
+        res.send(err);
+      }
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const userName = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
+
   User.findOne({ email: userName }, (err, foundUser) => {
     if (err) {
       console.log(err);
+    } else{
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            res.send("Email or password is wrong!");
+          }
+      });
     }
-    if (foundUser) {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      } else {
-        res.send("Email or password is wrong!");
-      }
-    }
+    
+  }
   });
 });
 
